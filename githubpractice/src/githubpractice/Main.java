22dba6659b26e33a8f5f5e02f584f63cd550e8eb
ryumh12 <danielmhryu@gmail.com>
2017-12-26 18:36:46 -0500

    Initial checkin of practice

---
+++ githubpractice/src/githubpractice/Main.java
@@ -0,0 +1,15 @@
+package githubpractice;
+
+class Greet{
+	
+	void greet(){
+		System.out.println("Hello World!");
+	}
+}
+public class Main {
+	public static void main (String[] args){
+		Greet gr = new Greet();
+		gr.greet();
+	}
+
+}
